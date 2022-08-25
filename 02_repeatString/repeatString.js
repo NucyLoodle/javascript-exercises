27c2f0ef63c2435fe0d991b44fb352376ac9ae09
Loodle <lucymeredith3@gmail.com>
2022-08-25 16:58:17 +0100

    Use if to return error

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,19 @@
 const repeatString = function(string, num) {
     let repeatedString = "";
-
+    if (num < 0) {
+        return 'ERROR'
+    } else {
     while (num > 0) {
         repeatedString += string;
         num --;
     }
     return repeatedString;
+}
+    
+    
     
 };
-repeatString("hey", 0);
+repeatString("hey", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
