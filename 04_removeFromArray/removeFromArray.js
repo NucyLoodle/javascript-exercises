b1e83fdb2cdb52ae9fde51f3241ee51e0c0a1211
Loodle <lucymeredith3@gmail.com>
2022-08-26 07:44:35 +0100

    write function to remove value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,value) {
+    const index = array.indexOf(value)
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    return array
+
 
 };
+removeFromArray([1,2,3,4],3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
