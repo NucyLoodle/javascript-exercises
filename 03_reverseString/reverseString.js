0e800e523b1577f96901cc7284909d1194a40e33
Loodle <lucymeredith3@gmail.com>
2022-08-26 07:15:45 +0100

    Test function with multiple words

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,8 @@
 const reverseString = function(string) {
     let stringArray = string.split("");
     return stringArray.reverse().join("");
-
 };
 
-reverseString("hello");
+reverseString("hello there");
 // Do not edit below this line
 module.exports = reverseString;
