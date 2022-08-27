75766f28f67bd35eb720d01f96a225d909c6cea2
Loodle <lucymeredith3@gmail.com>
2022-08-27 17:48:30 +0100

    Write second function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -5,7 +5,10 @@ const ftoc = function(fahrenheit) {
 
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let fahrenheit = ((celsius * (9/5)) + 32)
+  let roundedFahrenheit = Math.round(fahrenheit *10)/10
+  return roundedFahrenheit
 
 };
 
