749b4c8246a8ea4a92f10fd27f0674a7c1cf050c
Loodle <lucymeredith3@gmail.com>
2022-08-27 08:08:33 +0100

    Check if parameters are numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,19 @@
 const sumAll = function(num1, num2) {
-    if (num1 > 0 && num2 > 0) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR" 
+    } else if (num1 > 0 && num2 > 0) {
 
-        let maxNum = Math.max(num1, num2)
-        let minNum = Math.min(num1,num2)
-    
-        let sum = 0
-        for (let i = minNum; i <= maxNum; i++){
-            sum += i
-        }
-        return sum;
+            let maxNum = Math.max(num1, num2)
+            let minNum = Math.min(num1,num2)
+        
+            let sum = 0
+            for (let i = minNum; i <= maxNum; i++){
+                sum += i
+            }
 
+            return sum;
 
-    } else {
+            } else {
         return "ERROR";
     }
    
